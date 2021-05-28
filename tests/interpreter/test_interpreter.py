3bb6054c07258b508a7645745e2759ff879e9cf2
git-afsantos <contact.andre.santos@gmail.com>
2021-05-28 14:51:31 +0100

    placeholder for interpreter tests

---
+++ tests/interpreter/test_interpreter.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# SPDX-License-Identifier: MIT
+# Copyright © 2021 André Santos
+
+###############################################################################
+# Imports
+###############################################################################
+
+from pathlib import Path
+
+from hypothesis import given
+
+from haroslaunch.launch_interpreter import (
+    LaunchInterpreter, LaunchInterpreterError
+)
+
+###############################################################################
+# Tests
+###############################################################################
