2f5acfe49f42cbecabce77d64f0fc4ce2b0f35da
git-afsantos <contact.andre.santos@gmail.com>
2021-05-31 09:35:01 +0100

    refining tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -93,8 +93,99 @@ def test_kobuki_minimal():
     lfi.interpret(fp)
     assert not lfi.machines
     assert not lfi.rosparam_cmds
-    assert len(lfi.nodes) > 0
+    assert len(lfi.nodes) == 3
     assert len(lfi.parameters) > 0
+    # Node 0 -------------------------------
+    node = lfi.nodes[0]
+    assert not node.is_test_node
+    assert node.name.own == 'mobile_base_nodelet_manager'
+    assert node.name.full == '/mobile_base_nodelet_manager'
+    assert node.system is None
+    assert node.condition.is_true
+    assert node.traceability.filepath.endswith('/kobuki_minimal.launch')
+    assert node.traceability.line == 4
+    assert node.traceability.column == 4
+    assert node.package == 'nodelet'
+    assert node.executable == 'nodelet'
+    assert node.machine is None
+    assert node.is_required.value is False
+    assert node.respawns.value is False
+    assert node.respawn_delay.value == 0.0
+    assert node.args.value == 'manager'
+    assert node.output.value == 'log'
+    assert node.working_dir.value == 'ROS_HOME'
+    assert node.launch_prefix is None
+    assert len(node.remaps) == 0
+    assert len(node.environment) == 0
+    # Node 1 -------------------------------
+    node = lfi.nodes[1]
+    assert not node.is_test_node
+    assert node.name.own == 'mobile_base'
+    assert node.name.full == '/mobile_base'
+    assert node.system is None
+    assert node.condition.is_true
+    assert node.traceability.filepath.endswith('/kobuki_minimal.launch')
+    assert node.traceability.line == 5
+    assert node.traceability.column == 4
+    assert node.package == 'nodelet'
+    assert node.executable == 'nodelet'
+    assert node.machine is None
+    assert node.is_required.value is False
+    assert node.respawns.value is False
+    assert node.respawn_delay.value == 0.0
+    assert node.args.value == 'load kobuki_node/KobukiNodelet mobile_base_nodelet_manager'
+    assert node.output.value == 'log'
+    assert node.working_dir.value == 'ROS_HOME'
+    assert node.launch_prefix is None
+    assert len(node.remaps) == 2
+    assert node.remaps['/mobile_base/odom'].get_value() == '/odom'
+    assert node.remaps['/mobile_base/joint_states'].get_value() == '/joint_states'
+    assert len(node.environment) == 0
+    # Node 2 -------------------------------
+    node = lfi.nodes[2]
+    assert not node.is_test_node
+    assert node.name.own == 'diagnostic_aggregator'
+    assert node.name.full == '/diagnostic_aggregator'
+    assert node.system is None
+    assert node.condition.is_true
+    assert node.traceability.filepath.endswith('/kobuki_minimal.launch')
+    assert node.traceability.line == 12
+    assert node.traceability.column == 4
+    assert node.package == 'diagnostic_aggregator'
+    assert node.executable == 'aggregator_node'
+    assert node.machine is None
+    assert node.is_required.value is False
+    assert node.respawns.value is False
+    assert node.respawn_delay.value == 0.0
+    assert node.args.value == ''
+    assert node.output.value == 'log'
+    assert node.working_dir.value == 'ROS_HOME'
+    assert node.launch_prefix is None
+    assert len(node.remaps) == 0
+    assert len(node.environment) == 0
+    # Parameters ---------------------------
+    params = {
+        '/mobile_base/device_port': '/dev/kobuki',
+        '/mobile_base/wheel_left_joint_name': 'wheel_left_joint',
+        '/mobile_base/wheel_right_joint_name': 'wheel_right_joint',
+        '/mobile_base/battery_capacity': 16.5,
+        '/mobile_base/battery_low': 14.0,
+        '/mobile_base/battery_dangerous': 13.2,
+        '/mobile_base/cmd_vel_timeout': 0.6,
+        '/mobile_base/publish_tf': True,
+        '/mobile_base/use_imu_heading': True,
+        '/mobile_base/odom_frame': 'odom',
+        '/mobile_base/base_frame': 'base_footprint',
+    }
+    for p in lfi.parameters:
+        assert p.namespace == '/mobile_base'
+        assert p.system is None
+        assert p.condition.is_true
+        assert p.traceability.filepath.endswith('/kobuki_minimal.launch')
+        assert p.traceability.line == 6 or (p.traceability.line == 7
+            and p.name.full == '/mobile_base/publish_tf')
+        assert p.traceability.column == 6
+        assert p.value.value == params[p.name.full]
 
 
 def test_kobuki_safe_keyop():
